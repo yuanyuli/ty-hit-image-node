e78a3a1e4fcb5a6d5207945ae561ca7694f2f722
yuanyuli <zt_halcyon@yeah.net>
2026-09-12 00:37:02 +0800

    test: configure shared uv environment and package import

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
-from .nodes import CivitaiInspirationLoader
+try:
+    from .nodes import CivitaiInspirationLoader
+except ImportError:
+    from nodes import CivitaiInspirationLoader
 
 NODE_CLASS_MAPPINGS = {"CivitaiInspirationLoader": CivitaiInspirationLoader}
 NODE_DISPLAY_NAME_MAPPINGS = {"CivitaiInspirationLoader": "Civitai 灵感图加载器"}
